4c33663af23eb655fc231d7f2a52fea4811ebadc
LENOVO <LENOVO@192.168.1.36>
2025-02-12 16:26:06 +0530

    by aarti

---
+++ GitPractice/src/com/demo/Practice.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Practice 
+{
+	public void add()
+	{
+		System.out.println("This is add method");
+	}
+ public static void main(String[] args) 
+ {
+	 System.out.println("This is main method");
+	
+}
+}
